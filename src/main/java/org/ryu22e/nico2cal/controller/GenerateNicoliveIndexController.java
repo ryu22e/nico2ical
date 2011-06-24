5320d873f50839aec08a3ad2de356a2e5b425de0
ryu22e@gmail.com <ryu22e@gmail.com>
2011-06-24 17:24:56 +0900

    ログ出力処理を追加した。

--- src/main/java/org/ryu22e/nico2cal/controller/GenerateNicoliveIndexController.java
+++ src/main/java/org/ryu22e/nico2cal/controller/GenerateNicoliveIndexController.java
@@ -1,9 +1,13 @@
 package org.ryu22e.nico2cal.controller;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.ryu22e.nico2cal.model.Nicolive;
 import org.ryu22e.nico2cal.service.NicoliveService;
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
+import org.slim3.controller.validator.Errors;
 import org.slim3.controller.validator.Validators;
 import org.slim3.datastore.Datastore;
 
@@ -13,6 +17,12 @@ import org.slim3.datastore.Datastore;
  *
  */
 public final class GenerateNicoliveIndexController extends Controller {
+
+    /**
+     * 
+     */
+    private static final Logger LOGGER = Logger
+        .getLogger(GenerateNicoliveIndexController.class.getName());
 
     /**
      * 
@@ -29,21 +39,30 @@ public final class GenerateNicoliveIndexController extends Controller {
      */
     @Override
     public Navigation run() throws Exception {
+        LOGGER.info("BEGIN: " + this.getClass().getName());
+
         Validators v = new Validators(request);
         v.add("keys[]", v.required());
         if (!v.validate()) {
             response.setStatus(UNAUTHORIZED);
-            return null;
-        }
-        String[] keysString = request.getParameterValues("keys[]");
-        for (String key : keysString) {
-            Nicolive nicolive =
-                    nicoliveService.find(Datastore.stringToKey(key));
-            if (nicolive != null) {
-                nicoliveService.createIndex(nicolive);
+            if (LOGGER.isLoggable(Level.WARNING)) {
+                Errors errors = v.getErrors();
+                for (int i = 0; i < errors.size(); i++) {
+                    LOGGER.warning(errors.get(i));
+                }
+            }
+        } else {
+            String[] keysString = request.getParameterValues("keys[]");
+            for (String key : keysString) {
+                Nicolive nicolive =
+                        nicoliveService.find(Datastore.stringToKey(key));
+                if (nicolive != null) {
+                    nicoliveService.createIndex(nicolive);
+                }
             }
         }
 
+        LOGGER.info("END: " + this.getClass().getName());
         return null;
     }
 }
