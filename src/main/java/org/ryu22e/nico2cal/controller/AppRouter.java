8962841e2c7a0bb386b5bdba4ec10d50bf14b3fb
ryu22e@gmail.com <ryu22e@gmail.com>
2011-06-29 20:21:52 +0900

    コメントを書いた。

--- src/main/java/org/ryu22e/nico2cal/controller/AppRouter.java
+++ src/main/java/org/ryu22e/nico2cal/controller/AppRouter.java
@@ -3,10 +3,14 @@ package org.ryu22e.nico2cal.controller;
 import org.slim3.controller.router.RouterImpl;
 
 /**
+ * URLルーティングを設定するクラス。
  * @author ryu22e
  *
  */
 public class AppRouter extends RouterImpl {
+    /**
+     * コンストラクタ。
+     */
     public AppRouter() {
         addRouting(
             "/ical/{startWeek}/{keyword}",
